0a3b4e13269605b584f00c3fdd6af437721629b1
hgbao209 <hoanggiabaor146@gmail.com>
2026-07-02 01:17:50 +0700

    fix floatbutton

--- ios/Runner/AppDelegate.m
+++
@@ -1,8 +0,0 @@
-#include "AppTestAppDelegate.h"
-#include <Flutter/Flutter.h>
-
-@implementation AppTestAppDelegate
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  return YES;
-}
-@end
--- ios/Runner/main.m
+++
@@ -1,7 +0,0 @@
-#import "AppTestAppDelegate.h"
-
-int main(int argc, char *argv[]) {
-  @autoreleasepool {
-    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppTestAppDelegate class]));
-  }
-}
